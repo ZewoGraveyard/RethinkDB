2231c62f4e0e7304d3709fec91926c05728fbd97
Robert Payne <robertpayne@me.com>
2016-10-04 03:32:17 +1300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "RethinkDB",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/Zewo/Axis.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 14),
     ]
 )
